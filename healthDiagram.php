eee189fa2db93079ff9812b59e009ad273d490d9
ArjG <33728398+ArjG@users.noreply.github.com>
2020-03-03 17:08:12 +0000

    graphs

--- healthDiagram.php
+++ healthDiagram.php
@@ -6,11 +6,13 @@
   $user_id = "1";                     //NEEDS to change depending on the logged in user
 
   if ($sType != "exercise_done"){
-    $sql = "SELECT $sType FROM healthData WHERE user_id = $user_id";
+    $sql = "SELECT $sType, date_stored FROM healthData WHERE user_id = $user_id";
   } else {
     $sql = "SELECT $sType, exercise_time FROM healthData WHERE user_id = $user_id";
   }
   $stmt = $pdo->query($sql);
+
+  $dataP = array();
 ?>
 
 <!DOCTYPE html>
@@ -52,17 +54,29 @@
 <body>
     <div style="text-align:center" id="container">
         <h1>Hello world</h1>
+        <select id="select">
+        <option value="line">line</option>
+        <option value="bar">bar</option>
+        <option value="pie">pie</option>
+        <option value="scatter">scatter</option>
+        </select>
         <br>
 
         <!-- PHP for displaying the raw data -->
         <?php 
         if($sType == "heart_rate"){
 				  while($row =$stmt->fetchObject()){
-            echo "<p> Heart Rate: <b> $row->heart_rate </b></p>"; 
+            // echo "<p> Exercise Name: <b> $row->heart_rate </b> --
+            //           Date: <b> $row->date_stored </b></p>"; 
+
+            array_push($dataP, array( "y"=>$row->heart_rate));
           }
         } elseif ($sType == "hours_slept") {
           while($row =$stmt->fetchObject()){
-            echo "<p> Hours Slept: <b> $row->hours_slept </b></p>"; 
+            echo "<p> Exercise Name: <b> $row->hours_slept </b> --
+                      Date: <b> $row->date_stored </b></p>"; 
+            
+            array_push($dataP, array("x"=>$row->date_stored, "y"=>$row->hours_slept));
           }
         } elseif ($sType == "exercise_done") {
             while($row =$stmt->fetchObject()){
@@ -71,7 +85,46 @@
             }
         }?>
     
-  </div>
+<script> 
+
+
+window.onload = function () {
+
+  var year = "line";
+  $("#select" ).on('change', function() { 
+  year = $(this).val();
+  console.log(year);
+  });
+
+console.log("outside: ", year)
+var chartType = "line";
+var titleMsg = "<?php echo $sType; ?>"
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: titleMsg
+	},
+	axisY:{
+		includeZero: false
+	},
+	data: [{        
+		type: "line",
+      	indexLabelFontSize: 16,
+		dataPoints: <?php echo json_encode($dataP, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+}
+
+</script>
+
+<div id="chartContainer" style="height: 370px; width: 60%; padding-left: 20%"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+</div>
 </body>
 
 <footer style="padding-top:3%" class="container-fluid text-center">
