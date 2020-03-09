650fed66fe274ff70ae0aa1a25400e677f443bb4
Benjie1741 <benjie1741@yahoo.co.uk>
2020-03-09 16:36:31 +0000

    Added Arj's branch to work on CRUD functions
    Created new branch to create CRUD Functions for the application

---
+++ E-Health-System-Arj-Branch/includes/conn.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=ehealth2';
+$user = 'root';
+$password = '';
+try { 
+$pdo = new PDO($dsn, $user, $password); 
+$pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+$pdo ->exec("SET CHARACTER SET utf8");
+}
+catch (PDOException $e) { 
+echo 'Connection failed again: ' . $e->getMessage();
+}
+?>
