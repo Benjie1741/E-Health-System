43983533447b87639d9be78e292635a4d36fb71f
Benjie1741 <benjie1741@yahoo.co.uk>
2020-03-09 15:48:39 +0000

    Update e-healthsystem.sql
    Added data to healthData table and fixed date for healthData table

--- e-healthsystem.sql
+++ e-healthsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2020 at 05:20 PM
+-- Generation Time: Mar 09, 2020 at 05:53 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -67,8 +67,19 @@ CREATE TABLE `healthdata` (
   `hourOfExercise` int(255) NOT NULL,
   `heartRate` int(255) NOT NULL,
   `exerciseDone` char(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `dateOfExercise` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `healthdata`
+--
+
+INSERT INTO `healthdata` (`HealthDataID`, `userID`, `hoursOfSleep`, `hourOfExercise`, `heartRate`, `exerciseDone`, `dateOfExercise`) VALUES
+(1, 1, 7, 1, 75, 'Running', '2020-03-08'),
+(2, 2, 8, 0, 74, 'N/A', '2020-03-08'),
+(3, 3, 10, 2, 78, 'Weight lifting, Running', '2020-03-06'),
+(4, 4, 7, 1, 75, 'Yoga', '2020-03-08'),
+(5, 5, 3, 0, 75, 'N/A', '2020-03-08');
 
 -- --------------------------------------------------------
 
@@ -99,7 +110,11 @@ CREATE TABLE `patients` (
 --
 
 INSERT INTO `patients` (`PatientID`, `firstName`, `lastName`, `dateOfBirth`, `age`, `address`, `phoneNumber`, `email`, `password`, `bloodType`, `medicalHistory`, `illness`, `allergies`, `doctorID`, `prescription`) VALUES
-(1, 'Constance', 'Witmann', '2020-03-06', 21, 'Reutlingen, Germany', 1254789632, 'exchangeStudent@test.com', 'Const123', 'AB+', 'N/A', 'Corona Virus', 'Peanuts', 1, 'Paracetamol');
+(1, 'Constance', 'Witmann', '2020-03-06', 21, 'Reutlingen, Germany', 1254789632, 'exchangeStudent@test.com', 'Const123', 'AB+', 'N/A', 'Corona Virus', 'Peanuts', 1, 'Paracetamol'),
+(2, 'Juan', 'Juarez', '2020-03-01', 42, 'Mexico City, Mexico', 1258964736, 'CoronaIsADrink@NotAVirus.com', 'Juan23', 'A+', 'Medical History:\r\nMeningitis - 2012\r\nLeft arm amputation - 2018', 'Ebola', 'None', 4, 'Tequila'),
+(3, 'Andros', 'Konstantinos', '2020-03-05', 47, 'Athens, Greece', 2574819463, 'AK-47@MyInitials.com', 'ak47', 'O-', 'Medical History:\r\nAlcoholism - 1980\r\nSubstance Abuse - 1990\r\n', 'Early onset of frostbite', 'None', 2, 'A heat pack. RIP'),
+(4, 'Amy', 'Whitley', '2020-03-06', 28, 'Fort Worth, Texas, USA', 1299854763, 'MakeAmerica@ConnectedAgain.com', 'MAGA123', 'B+', 'Medical History:\r\nRemoval of two wisdom teeth - 2011\r\n', 'High fever, Burning eyes, nose bleeds, nausea, aches and pains, hair loss, Random anaphylaxis', 'Dogs, cats, grass, peanut butter, fun', 3, 'Glass of water'),
+(5, 'Anatoly', 'Karpov', '2020-03-04', 44, 'Svastopol, Russia', 1258947635, 'Vodka@me.com', 'Ana123', 'AB-', 'Medical History:\r\nSEVERE alcohol poisoning: 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010', 'Alcohol Posioning', 'Water', 1, 'Blood transfusion');
 
 --
 -- Indexes for dumped tables
@@ -141,13 +156,13 @@ ALTER TABLE `doctors`
 -- AUTO_INCREMENT for table `healthdata`
 --
 ALTER TABLE `healthdata`
-  MODIFY `HealthDataID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `HealthDataID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `PatientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `PatientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
