5ae705b2a0f3d74e48213f5e8dce64a021b7f76a
Benjie1741 <benjie1741@yahoo.co.uk>
2020-03-14 17:18:20 +0000

    Update editHealthData.php
    Didnt need to use userID for edit

--- editHealthData.php
+++ editHealthData.php
@@ -2,7 +2,6 @@
 require('includes/conn.inc.php');
 
 $healthDataID = $_POST['hid'];
-$userID = $_POST['pid'];
 $hoursOfSleep = $_POST['hoursOfSleep'];
 $hoursOfExercise = $_POST['hoursOfExercise'];
 $heartRate = $_POST['heartRate'];
@@ -11,7 +10,7 @@ $dateOfExercise = $_POST['doe'];
 
 try {
 	$sql = "UPDATE healthdata 
-			SET healthDataID=:healthDataID, userID=:userID,
+			SET healthDataID=:healthDataID,
 				hoursOfSleep=:hoursOfSleep, hoursOfExercise=:hoursOfExercise,
 				heartRate=:heartRate, exerciseDone=:exerciseDone,
 				dateOfExercise=:dateOfExercise
@@ -20,7 +19,6 @@ try {
             $stmt = $pdo->prepare($sql);
 
             $stmt->bindParam(':healthDataID', $healthDataID);
-            $stmt->bindParam(':userID', $userID);
             $stmt->bindParam(':hoursOfSleep', $hoursOfSleep);
             $stmt->bindParam(':hoursOfExercise', $hoursOfExercise);
             $stmt->bindParam(':heartRate', $heartRate);
