4807d090984bb6d92b9d5c285291028fec008f7d
Gustavo Sánchez <39857935+gus5298@users.noreply.github.com>
2020-03-14 15:30:29 +0000

    update insertDoc with new sql query

--- insertDoc.php
+++ insertDoc.php
@@ -3,16 +3,24 @@ include('includes/conn.inc.php');
 
 $email = $_POST['email'];
 
-$password = $_POST['password'];
-//$doctorID = $_POST['docID'];
+$userPassword = $_POST['password'];
 
+$firstName = $_POST['firstname'];
+$lastName = $_POST['lastname'];
+$age = $_POST['age'];
+$userAddress = $_POST['address'];
+$num = $_POST['num'];
+$clearance = $_POST['clearance'];
+$dob = $_POST['dob'];
+$license = $_POST['license'];
+$specialty = $_POST['specialty'];
 
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+$hashed_password = password_hash($userPassword, PASSWORD_DEFAULT);
 
 try {
 
-	$sql= "INSERT INTO doctors (email, userPassword)
-       VALUES ('$email', '$hashed_password')";
+	$sql= "INSERT INTO doctors (email, password, firstName, lastName, dateOfBirth, age, userAddress, phoneNumber, clearanceLevel, licenseRevalidationDate, specialty)
+       VALUES ('$email', '$hashed_password', '$firstName', '$lastName', '$dob', '$age', '$userAddress', '$num', '$clearance', '$license', '$specialty')";
        $stmt = $pdo -> query($sql);
        header("Location: ./homeDoc.php");
 }catch (\Exception $e) {
