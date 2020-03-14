f0f65c79d98398d83173b6e6f4755a30d48947f9
Gustavo Sánchez <39857935+gus5298@users.noreply.github.com>
2020-03-14 15:33:43 +0000

    update insertPat with new sql query

--- insertPat.php
+++ insertPat.php
@@ -5,17 +5,27 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 $email = $_POST['email'];
-
-$password = $_POST['password'];
+$userPassword = $_POST['password'];
 $doctorID = $_POST['docID'];
+$firstName = $_POST['firstname'];
+$lastName = $_POST['lastname'];
+$age = $_POST['age'];
+$userAddress = $_POST['address'];
+$num = $_POST['num'];
+$blood = $_POST['blood'];
+$dob = $_POST['dob'];
+$history = $_POST['history'];
+$illness = $_POST['illness'];
+$prescription = $_POST['prescription'];
+$allergies = $_POST['allergies'];
 
 
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 try {
 
-	$sql= "INSERT INTO patients (email, userPassword, doctorID)
-       VALUES ('$email', '$hashed_password', '$doctorID')";
+       $sql= "INSERT INTO patients (email, userPassword, doctorID, firstName, lastName, dateOfBirth, age, userAddress, phoneNumber, bloodType, medicalHistory, allergies, prescription, illness)
+       VALUES ('$email', '$hashed_password', '$doctorID', '$firstName', '$lastName', '$dob', '$age', '$userAddress', '$num', '$blood', '$history', '$allergies', '$prescription', '$illness')";
        $stmt = $pdo -> query($sql);
 	   header("Location: ./homeDoc.php");
     }catch (\Exception $e) {
