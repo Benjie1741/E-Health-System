9f1cb5c04b6bb4e8d824f33818b35bcd784f7886
Benjie1741 <benjie1741@yahoo.co.uk>
2020-03-14 16:03:18 +0000

    Add files via upload
    Added CRUD functionality for both patient and doctor

---
+++ insertHealthData.php
@@ -0,0 +1,46 @@
+<?php
+require('includes/conn.inc.php');
+session_start();
+
+$userID = $_SESSION['chatID'];
+$hoursOfSleep = $_POST['hoursOfSleep'];
+$hoursOfExercise = $_POST['hoursOfExercise'];
+$heartRate = $_POST['heartRate'];
+$exerciseDone = $_POST['exerciseDone'];
+$dateOfExercise = $_POST['doe'];
+
+print $userID;
+
+try {
+	$sql=   "INSERT INTO healthdata 
+            (userID, hoursOfSleep, hoursOfExercise, 
+            heartRate, exerciseDone, dateOfExercise)
+            VALUES 
+            ($userID, $hoursOfSleep, $hoursOfExercise,
+            $heartRate, '$exerciseDone', '$dateOfExercise')";
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindParam(':userID', $userID);
+            $stmt->bindParam(':hoursOfSleep', $hoursOfSleep);
+            $stmt->bindParam(':hoursOfExercise', $hoursOfExercise);
+            $stmt->bindParam(':heartRate', $heartRate);
+            $stmt->bindParam(':exerciseDone', $exerciseDone);
+            $stmt->bindParam(':dateOfExercise', $dateOfExercise);
+
+            $stmt->execute();
+
+	        header("Location: ../eHealth1/homePat.php");
+}catch (\Exception $e) {
+
+        print $e;
+
+        $message = $e;
+        echo "<script type='text/javascript'>alert('$message');
+        location.href = 'homeDoc.php';
+        </script>";
+        //header("Location: ../eHealth/homeDoc.php");
+
+//		header("Location: ../eHealth/register.php");
+}
+
+?>
